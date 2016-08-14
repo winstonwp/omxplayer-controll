7f1034812740fe43bc203731f96d034a7b3c8e31
winstonwp <winstonwp@gmail.com>
2016-08-13 21:09:31 -0600

    Easier to understand example

--- examples/simple_test.js
+++ examples/simple_test.js
@@ -1,24 +1,7 @@
 /*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
-var fs = require('fs');
 var omxp = require('../index');
 
-var files = ['/home/pi/test1.mp3', '/home/pi/test2.mp3'],
-    files_pos = 0;
-
-var opts_transitiong = {
-    'audioOutput': 'hdmi',
-    'blackBackground': false,
-    'disableKeys': true,
-    'disableOnScreenDisplay': true,
-    'disableGhostbox': true,
-    'subtitlePath': '',
-    'startAt': 0,
-    'startVolume': 0.8,
-    'maxPlayerAllowCount': 2, //This would allow to open a second player from transitioning. Defaults to 1
-    'closeOtherPlayers': false //Leave players open if there are 2 (or more)
-};
-
-var opts_single = {
+var opts = {
     'audioOutput': 'hdmi',
     'blackBackground': false,
     'disableKeys': true,
@@ -32,32 +15,15 @@ var opts_single = {
 
 
 omxp.on('changeStatus', function(status) {
-    console.log('Status', status);
+    console.log('Status', JSON.stringify(status));
+    if (status.pos < 100000000)
+        omxp.setPosition(0.95 * status.duration / 10000);
 });
 omxp.on('aboutToFinish', function() {
-    //For example to do a transitiong
-    omxp.open(select_file(false), opts_transitiong);
+    console.log('========= About To Finish ==========');
 });
 omxp.on('finish', function() {
-    omxp.playersRunning(function(err, count) {
-        if (count === 0) {
-            omxp.open(select_file(false), opts_single);
-        } else {
-            console.log('Previous track finished,  but new one is running');
-        }
-    });
+    console.log('============= Finished =============');
+    omxp.open('/home/pi/test1.mp3', opts);
 });
-omxp.open(select_file(false), opts_single);
-
-function select_file(isRetry) {
-    var sf_pos = files_pos + 1 < files.length ? files[files_pos++] : files[0];
-    if (fs.existsSync(files[sf_pos])) {
-        return files[sf_pos];
-    } else {
-        if (!isRetry) {
-            return select_file(true);
-        } else {
-            return null;
-        }
-    }
-}
+omxp.open('/home/pi/test1.mp3', opts);
