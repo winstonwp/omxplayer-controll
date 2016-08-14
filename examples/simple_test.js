ef2e9549a4ce14a184b90ffb2bc000cda372fd96
winstonwp <winstonwp@gmail.com>
2016-08-13 21:08:38 -0600

    Fixing README.md

--- examples/simple_test.js
+++ examples/simple_test.js
@@ -2,7 +2,10 @@
 var fs = require('fs');
 var omxp = require('../index');
 
-var opts = {
+var files = ['/home/pi/test1.mp3', '/home/pi/test2.mp3'],
+    files_pos = 0;
+
+var opts_transitiong = {
     'audioOutput': 'hdmi',
     'blackBackground': false,
     'disableKeys': true,
@@ -10,17 +13,51 @@ var opts = {
     'disableGhostbox': true,
     'subtitlePath': '',
     'startAt': 0,
-    'startVolume': 0.8
+    'startVolume': 0.8,
+    'maxPlayerAllowCount': 2, //This would allow to open a second player from transitioning. Defaults to 1
+    'closeOtherPlayers': false //Leave players open if there are 2 (or more)
 };
-var file = '/home/pi/test.mp3';
-if(fs.existsSync(file)){
-    omxp.open(file, opts);
-    omxp.on('changeStatus', function(status) {
-        console.log('Status', status);
+
+var opts_single = {
+    'audioOutput': 'hdmi',
+    'blackBackground': false,
+    'disableKeys': true,
+    'disableOnScreenDisplay': true,
+    'disableGhostbox': true,
+    'subtitlePath': '',
+    'startAt': 0,
+    'startVolume': 0.8,
+    'closeOtherPlayers': true //Should close other players if necessary
+};
+
+
+omxp.on('changeStatus', function(status) {
+    console.log('Status', status);
+});
+omxp.on('aboutToFinish', function() {
+    //For example to do a transitiong
+    omxp.open(select_file(false), opts_transitiong);
+});
+omxp.on('finish', function() {
+    omxp.playersRunning(function(err, count) {
+        if (count === 0) {
+            omxp.open(select_file(false), opts_single);
+        } else {
+            console.log('Previous track finished,  but new one is running');
+        }
     });
-    omxp.on('aboutToFinish', function() {
-        console.log('About To Finish');
-    });
-}else{
-    console.error(file + ' non existing');
+});
+omxp.open(select_file(false), opts_single);
+
+function select_file(isRetry) {
+    var sf_pos = files_pos + 1 < files.length ? files[files_pos++] : files[0];
+    if (fs.existsSync(files[sf_pos])) {
+        return files[sf_pos];
+    } else {
+        if (!isRetry) {
+            return select_file(true);
+        } else {
+            return null;
+        }
+    }
 }
